dfa67c9e659998c38e472ec78d17108202324e32
SolarPowerWu <solarpowerwu@gmail.com>
2023-08-01 23:52:35 -0500

    Beckers Rixen has train and test now

--- BeckersRixen.py
+++ BeckersRixen.py
@@ -5,14 +5,15 @@ def BeckersRixen(M0, test, nmin=1, nmax=None, val_perc=10, tol=1e-4):
     sm = M0.shape
     m, n = sm
     test_m, test_n = test.shape
+    assert m == test_m
     
     if nmax is None:  # Default number of modes to use
         nmax = round(np.min(sm) / 2)
         
     nmax = min(np.min(sm) - 1, nmax)
     
-    # Count the zeros by row
-    # zcount = np.sum(M0 == 0, axis=1) / n
+    # Count the zeros by row, want to match ratio of zeros in test set to ensure it works well
+    zcount = np.sum(test == 0, axis=1) / test_n
     
     # Add on the error estimate rows
     val_n = int(np.ceil(n * val_perc / 100))   # How many to train on?
@@ -21,20 +22,17 @@ def BeckersRixen(M0, test, nmin=1, nmax=None, val_perc=10, tol=1e-4):
     ndx = np.random.randint(n, size=val_n)   # Make a random guess of columns to include
     
     for ii in range(val_n):
-        while np.prod(M0[:, ndx[ii]]) == 0:
-            ndx[ii] = np.random.randint(n)  # Try again until you find a column without gaps
+        give_up = 100
+        while np.prod(M0[:, ndx[ii]]) == 0 and give_up > 0:
+            ndx[ii] = np.random.randint(n)  # Try again until you find a column without gaps (is this necessary?)
+            give_up -= 1
         Md[:, ndx[ii]] *= (np.random.rand(m) > zcount)  # Zeroing some
-    
-    #Remove testing columns from training columns
-    training_idx = np.array([True] * (n + added))
-    training_idx[ndx] = False
-    Md = Md[:, training_idx]
-    Mp = Mp[:, training_idx]
+
+    Md = np.concatenate((Md, test), axis = 1) # Append test set so it's also affected by the algorithm
 
     zdx = np.where(Md == 0)
-    adx = np.arange(n - added, n)
     
-    initerror = np.sum((Mp[:, adx] - Md[:, adx]) ** 2)  # The initial error, now let's reduce it!
+    initerror = np.sum((Mp[:, ndx] - Md[:, ndx]) ** 2)  # The initial error, now let's reduce it!
     itererror = initerror
     nerror = np.zeros(nmax)
     
@@ -52,11 +50,11 @@ def BeckersRixen(M0, test, nmin=1, nmax=None, val_perc=10, tol=1e-4):
             
             Mdnew = np.dot(U[:,:nnow + 1], np.dot(np.diag(S[:nnow + 1]), V[:nnow + 1]))  # Form the new guess
             
-            itererror = np.sum((Mp[:, adx] - Mdnew[:, adx]) ** 2)  # Find new error
-            olderror = np.sum((Mp[:, adx] - Md[:, adx]) ** 2)  # Find error update
+            itererror = np.sum((Mp[:, ndx] - Mdnew[:, ndx]) ** 2)  # Find new error
+            olderror = np.sum((Mp[:, ndx] - Md[:, ndx]) ** 2)  # Find error update
             
             olderrorup = errorup
-            errorup = np.sum((Mp[:, adx] - Md[:, adx]) ** 2) - itererror  # Find error update
+            errorup = np.sum((Mp[:, ndx] - Md[:, ndx]) ** 2) - itererror  # Find error update
             
         nerror[nnow-1] = itererror  # Check to ensure error is decreasing with increasing n
         if nnow > 1 and nerror[nnow-1] > nerror[nnow-2]:
@@ -64,9 +62,6 @@ def BeckersRixen(M0, test, nmin=1, nmax=None, val_perc=10, tol=1e-4):
         
         nnow += 1  # Take more nodes
     
-    revert_idx = np.arange(n)
-    revert_idx[ndx] = adx
-    revert_idx[adx] = ndx
-    Ma = Md[:, revert_idx]
+    Ma = Md
     
-    return Ma, U, S, V
+    return Ma
